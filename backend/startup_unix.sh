c5f97152ceca01332b89dd8989e2ba0f0cd9958a
hakimks <hakimks@gmail.com>
2026-05-20 15:52:13 +0300

    Fix port number

--- backend/startup_unix.sh
+++ backend/startup_unix.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Start Uvicorn server
-exec gunicorn -k uvicorn.workers.UvicornWorker -w 4 -b 0.0.0.0:5000 --preload app.main:app --timeout 120 --keep-alive 120 --log-level debug
+exec gunicorn -k uvicorn.workers.UvicornWorker -w 4 -b 0.0.0.0:${PORT:-6100} --preload app.main:app --timeout 120 --keep-alive 120 --log-level debug
